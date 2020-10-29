c9c53df6ef470518eb49f4ba3e15e57e95766dbd
okatanaaa <whitemarsstudios@gmail.com>
2020-10-29 22:35:49 +0400

    refactor: modified data normalization, block and models graph

--- tools.py
+++ tools.py
@@ -4,7 +4,8 @@ from tensorflow.keras.datasets import cifar10
 def load_cifar10():
     (Xtrain, Ytrain), (Xtest, Ytest) = cifar10.load_data()
     
-    X_mean, X_std = Xtrain.mean(), Xtrain.std()
+    Xtrain_flat = Xtrain.reshape(-1, 3)
+    X_mean, X_std = Xtrain_flat.mean(axis=0), Xtrain_flat.std(axis=0)
 
     Xtrain = (Xtrain - X_mean) / X_std
     Xtest = (Xtest - X_mean) / X_std
