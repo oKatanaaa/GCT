f83e48b1505d8ff8ec83f8d0492fbbc5e2c4c933
oKatanaaa <whitemarsstudios@gmail.com>
2021-01-08 11:41:22 -0800

    new(tools): load_boston_housing

--- gct/tools.py
+++ gct/tools.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.datasets import cifar10, boston_housing
 
 
 def load_cifar10():
@@ -6,6 +6,16 @@ def load_cifar10():
     
     Xtrain_flat = Xtrain.reshape(-1, 3)
     X_mean, X_std = Xtrain_flat.mean(axis=0), Xtrain_flat.std(axis=0)
+
+    Xtrain = (Xtrain - X_mean) / X_std
+    Xtest = (Xtest - X_mean) / X_std
+    return (Xtrain, Ytrain), (Xtest, Ytest), (X_mean, X_std)
+
+
+def load_boston_housing():
+    (Xtrain, Ytrain), (Xtest, Ytest) = boston_housing.load_data()
+
+    X_mean, X_std = Xtrain.mean(axis=0), Xtrain.std(axis=0)
 
     Xtrain = (Xtrain - X_mean) / X_std
     Xtest = (Xtest - X_mean) / X_std
